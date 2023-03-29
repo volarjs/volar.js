5e602ad65723452533e4c4f1f6b051a5ece3e73b
johnsoncodehk <johnsoncodehk@gmail.com>
2023-03-29 12:28:44 +0800

    fix: expand selection result incorrect
    close https://github.com/vuejs/language-tools/issues/2477

--- packages/language-service/src/documentFeatures/selectionRanges.ts
+++ packages/language-service/src/documentFeatures/selectionRanges.ts
@@ -2,7 +2,7 @@ import type { LanguageServicePluginContext } from '../types';
 import { languageFeatureWorker } from '../utils/featureWorkers';
 import * as transformer from '../transformer';
 import * as vscode from 'vscode-languageserver-protocol';
-import { notEmpty } from '../utils/common';
+import { isInsideRange, notEmpty } from '../utils/common';
 
 export function register(context: LanguageServicePluginContext) {
 
@@ -32,21 +32,37 @@ export function register(context: LanguageServicePluginContext) {
 			},
 			(item, map) => map ? transformer.asSelectionRanges(item, range => map.toSourceRange(range)) : item,
 			results => {
-				for (let i = 0; i < results[0].length; i++) {
-					const first = results[0][i];
-					let lastParent = first;
-					while (lastParent.parent) {
-						lastParent = lastParent.parent;
+				const result: vscode.SelectionRange[] = [];
+				for (let i = 0; i < positions.length; i++) {
+					let pluginResults: vscode.SelectionRange[] = [];
+					for (const ranges of results) {
+						pluginResults.push(ranges[i]);
 					}
-					for (let j = 1; j < results.length; j++) {
-						const other = results[j][i];
-						lastParent.parent = other;
-						while (lastParent.parent) {
-							lastParent = lastParent.parent;
+					pluginResults = pluginResults.sort((a, b) => {
+						if (isInsideRange(a.range, b.range)) {
+							return 1;
+						}
+						if (isInsideRange(b.range, a.range)) {
+							return -1;
+						}
+						return 0;
+					});
+					for (let i = 1; i < pluginResults.length; i++) {
+						let root = pluginResults[i - 1];
+						while (root.parent) {
+							root = root.parent;
+						}
+						let parent: vscode.SelectionRange | undefined = pluginResults[i];
+						while (parent && !isInsideRange(parent.range, root.range)) {
+							parent = parent.parent;
+						}
+						if (parent) {
+							root.parent = parent;
 						}
 					}
+					result.push(pluginResults[0]);
 				}
-				return results[0];
+				return result;
 			},
 		);
 	};
