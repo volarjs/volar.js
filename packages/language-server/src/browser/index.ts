e26917562b1148da99f47fbf5ef250e6c41d6d9c
Johnson Chu <johnsoncodehk@gmail.com>
2023-07-09 09:43:55 +0800

    perf(web): reduce fs requests

--- packages/language-server/src/browser/index.ts
+++ packages/language-server/src/browser/index.ts
@@ -3,7 +3,8 @@ import { startCommonLanguageServer } from '../common/server';
 import { LanguageServerPlugin } from '../types';
 import httpSchemaRequestHandler from '../common/schemaRequestHandlers/http';
 import { URI } from 'vscode-uri';
-import { FsReadFileRequest, FsReadDirectoryRequest, FsStatRequest } from '../protocol';
+import { FsReadFileRequest, FsReadDirectoryRequest } from '../protocol';
+import { FileSystem, FileType } from '@volar/language-service';
 
 export * from '../index';
 
@@ -40,35 +41,95 @@ export function startLanguageServer(connection: vscode.Connection, ...plugins: L
 			}
 			catch { }
 		},
-		fs: {
-			stat(uri) {
-				return connection.sendRequest(FsStatRequest.type, uri);
-			},
-			async readFile(uri) {
+		fs: createFs(connection),
+	}));
+}
+
+/**
+ * To avoid hitting the API hourly limit, we keep requests as low as possible.
+ */
+function createFs(connection: vscode.Connection): FileSystem {
+
+	const readDirectoryResults = new Map<string, Promise<[string, FileType][]>>();
+
+	return {
+		async stat(uri) {
+			if (uri.startsWith('__invalid__:')) {
+				return;
+			}
+			if (uri.startsWith('http://') || uri.startsWith('https://')) {
+				const text = await this.readFile(uri); // TODO: perf
+				if (text !== undefined) {
+					return {
+						type: FileType.File,
+						size: text.length,
+						ctime: -1,
+						mtime: -1,
+					};
+				}
+				return undefined;
+			}
+			const dirUri = uri.substring(0, uri.lastIndexOf('/'));
+			const baseName = uri.substring(uri.lastIndexOf('/') + 1);
+			const entries = await this.readDirectory(dirUri);
+			const matches = entries.filter(entry => entry[0] === baseName);
+			if (matches.length) {
+				return {
+					type: matches.some(entry => entry[1] === FileType.File) ? FileType.File : matches[0][1],
+					size: -1,
+					ctime: -1,
+					mtime: -1,
+				};
+			}
+		},
+		async readFile(uri) {
+			if (uri.startsWith('__invalid__:')) {
+				return;
+			}
+			if (uri.startsWith('http://') || uri.startsWith('https://')) {
+				return await httpSchemaRequestHandler(uri);
+			}
+			const dirUri = uri.substring(0, uri.lastIndexOf('/'));
+			const baseName = uri.substring(uri.lastIndexOf('/') + 1);
+			const entries = await this.readDirectory(dirUri);
+			const file = entries.filter(entry => entry[0] === baseName && entry[1] === FileType.File);
+			if (file) {
 				const text = await connection.sendRequest(FsReadFileRequest.type, uri);
 				if (text !== undefined && text !== null) {
 					return text;
 				}
-				if (uri.startsWith('http://') || uri.startsWith('https://')) {
-					return await httpSchemaRequestHandler(uri);
-				}
-			},
-			async readDirectory(uri) {
-				return connection.sendRequest(FsReadDirectoryRequest.type, uri);
-			},
+			}
 		},
-	}));
+		async readDirectory(uri) {
+			if (uri.startsWith('__invalid__:')) {
+				return [];
+			}
+			if (uri.startsWith('http://') || uri.startsWith('https://')) {
+				return [];
+			}
+			if (!readDirectoryResults.has(uri)) {
+				readDirectoryResults.set(uri, connection.sendRequest(FsReadDirectoryRequest.type, uri));
+			}
+			return await readDirectoryResults.get(uri)!;
+		},
+	};
 }
 
 function uriToFileName(uri: string) {
 	const parsed = URI.parse(uri);
+	if (parsed.scheme === '__invalid__') {
+		return parsed.path;
+	}
 	return `/${parsed.scheme}${parsed.authority ? '@' + parsed.authority : ''}${parsed.path}`;
-};
+}
 
 function fileNameToUri(fileName: string) {
 	const parts = fileName.split('/');
 	if (parts.length <= 1) {
-		return URI.from({ scheme: '', path: '' }).toString();
+		return URI.from({
+			scheme: '__invalid__',
+			path: fileName,
+		}).toString();
 	}
 	const firstParts = parts[1].split('@');
 	return URI.from({
@@ -76,4 +137,4 @@ function fileNameToUri(fileName: string) {
 		authority: firstParts.length > 1 ? firstParts[1] : undefined,
 		path: '/' + parts.slice(2).join('/'),
 	}).toString();
-};
+}
