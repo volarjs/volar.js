7359e19279a47652c081444a3eb52738aa6280d8
Johnson Chu <johnsoncodehk@gmail.com>
2023-10-08 23:42:44 +0800

    chore: update `@types/vscode`

---
+++ packages/vscode/tests/index.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from 'vitest';
+
+describe('vscode', () => {
+	test('vscode versions should be consistent ', () => {
+
+		const languageClientVersion = require('vscode-languageclient/package.json').engines.vscode;
+		const typesVersion = require('../package.json').devDependencies['@types/vscode'];
+
+		expect(typesVersion).toBe(languageClientVersion);
+	});
+});
