ad8c6a30ecf4e4ed1e8523d02951a69b4c232d76
Johnson Chu <johnsoncodehk@gmail.com>
2024-03-13 18:56:02 +0800

    Revert "refactor(typescript): remove `getDocumentRegistry` API"
    This reverts commit 99aa0208321b3a967a541bb89effa8d08607032a.

--- packages/typescript/index.ts
+++ packages/typescript/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/documentRegistry';
 export * from './lib/node/decorateLanguageService';
 export * from './lib/node/decorateLanguageServiceHost';
 export * from './lib/node/decorateProgram';
