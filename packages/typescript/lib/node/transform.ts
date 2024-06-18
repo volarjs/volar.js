e0ba773ff981171ad45a30eb79be28d1f8cef1e2
Johnson Chu <johnsoncodehk@gmail.com>
2024-06-19 04:21:09 +0800

    Update transform.ts

--- packages/typescript/lib/node/transform.ts
+++ packages/typescript/lib/node/transform.ts
@@ -245,9 +245,25 @@ export function* toSourceRanges(
 ): Generator<[fileName: string, start: number, end: number]> {
 	if (sourceScript) {
 		const map = language.maps.get(serviceScript.code, sourceScript);
+		let mapped = false;
 		for (const [sourceStart, sourceEnd, mapping] of map.getSourceStartEnd(start - getMappingOffset(language, serviceScript), end - getMappingOffset(language, serviceScript))) {
 			if (filter(mapping.data)) {
+				mapped = true;
 				yield [sourceScript.id, sourceStart, sourceEnd];
+			}
+		}
+		if (!mapped) {
+			// fallback
+			for (const sourceStart of toSourceOffsets(sourceScript, language, serviceScript, start, filter)) {
+				for (const sourceEnd of toSourceOffsets(sourceScript, language, serviceScript, end, filter)) {
+					if (
+						sourceStart[0] === sourceEnd[0]
+						&& sourceEnd[1] >= sourceStart[1]
+					) {
+						yield [sourceStart[0], sourceStart[1], sourceEnd[1]];
+						break;
+					}
+				}
 			}
 		}
 	}
@@ -297,13 +313,25 @@ export function* toGeneratedRanges(
 	filter: (data: CodeInformation) => boolean
 ) {
 	const map = language.maps.get(serviceScript.code, sourceScript);
+	let mapped = false;
 	for (const [generateStart, generateEnd, mapping] of map.getGeneratedStartEnd(start, end)) {
 		if (filter(mapping.data)) {
+			mapped = true;
 			yield [
 				generateStart + getMappingOffset(language, serviceScript),
 				generateEnd + getMappingOffset(language, serviceScript),
-				mapping,
 			] as const;
+		}
+	}
+	if (!mapped) {
+		// fallback
+		for (const [generatedStart] of toGeneratedOffsets(language, serviceScript, sourceScript, start, filter)) {
+			for (const [generatedEnd] of toGeneratedOffsets(language, serviceScript, sourceScript, end, filter)) {
+				if (generatedEnd >= generatedStart) {
+					yield [generatedStart, generatedEnd] as const;
+					break;
+				}
+			}
 		}
 	}
 }
