793743e1e873ee6a589added2b56afb1b44a50c5
Ray <i@mk1.io>
2024-07-08 08:27:38 +0800

    feat: export `replaceTscContent` for downstream usage

--- packages/typescript/lib/quickstart/runTsc.ts
+++ packages/typescript/lib/quickstart/runTsc.ts
@@ -7,6 +7,53 @@ export let getLanguagePlugins: (ts: typeof import('typescript'), options: ts.Cre
 	setup?(language: Language<string>): void,
 } = () => [];
 
+export function replaceTscContent(
+	tsc: string,
+	proxyApiPath: string,
+	extraSupportedExtensions: string[],
+	extraExtensionsToRemove: string[],
+	getLanguagePluginsFile?: string,
+) {
+	
+	const needPatchExtenstions = extraSupportedExtensions.filter(ext => !extraExtensionsToRemove.includes(ext));
+
+	// Add allow extensions
+	if (extraSupportedExtensions.length) {
+		const extsText = extraSupportedExtensions.map(ext => `"${ext}"`).join(', ');
+		tsc = replace(tsc, /supportedTSExtensions = .*(?=;)/, s => s + `.map((group, i) => i === 0 ? group.splice(0, 0, ${extsText}) && group : group)`);
+		tsc = replace(tsc, /supportedJSExtensions = .*(?=;)/, s => s + `.map((group, i) => i === 0 ? group.splice(0, 0, ${extsText}) && group : group)`);
+		tsc = replace(tsc, /allSupportedExtensions = .*(?=;)/, s => s + `.map((group, i) => i === 0 ? group.splice(0, 0, ${extsText}) && group : group)`);
+	}
+	// Use to emit basename.xxx to basename.d.ts instead of basename.xxx.d.ts
+	if (extraExtensionsToRemove.length) {
+		const extsText = extraExtensionsToRemove.map(ext => `"${ext}"`).join(', ');
+		tsc = replace(tsc, /extensionsToRemove = .*(?=;)/, s => s + `.concat([${extsText}])`);
+	}
+	// Support for basename.xxx to basename.xxx.d.ts
+	if (needPatchExtenstions.length) {
+		const extsText = needPatchExtenstions.map(ext => `"${ext}"`).join(', ');
+		tsc = replace(tsc, /function changeExtension\(/, s => `function changeExtension(path, newExtension) {
+			return [${extsText}].some(ext => path.endsWith(ext))
+				? path + newExtension
+				: _changeExtension(path, newExtension)
+			}\n` + s.replace('changeExtension', '_changeExtension'));
+	}
+
+	// proxy createProgram
+	tsc = replace(tsc, /function createProgram\(.+\) {/, s =>
+		`var createProgram = require(${JSON.stringify(proxyApiPath)}).proxyCreateProgram(`
+		+ [
+			`new Proxy({}, { get(_target, p, _receiver) { return eval(p); } } )`,
+			`_createProgram`,
+			`require(${JSON.stringify(getLanguagePluginsFile ?? __filename)}).getLanguagePlugins`,
+		].join(', ')
+		+ `);\n`
+		+ s.replace('createProgram', '_createProgram')
+	);
+
+	return tsc;
+}
+
 export function runTsc(
 	tscPath: string,
 	options: string[] | {
@@ -35,43 +82,8 @@ export function runTsc(
 				extraSupportedExtensions = options.extraSupportedExtensions;
 				extraExtensionsToRemove = options.extraExtensionsToRemove;
 			}
-			const needPatchExtenstions = extraSupportedExtensions.filter(ext => !extraExtensionsToRemove.includes(ext));
 
-			// Add allow extensions
-			if (extraSupportedExtensions.length) {
-				const extsText = extraSupportedExtensions.map(ext => `"${ext}"`).join(', ');
-				tsc = replace(tsc, /supportedTSExtensions = .*(?=;)/, s => s + `.map((group, i) => i === 0 ? group.splice(0, 0, ${extsText}) && group : group)`);
-				tsc = replace(tsc, /supportedJSExtensions = .*(?=;)/, s => s + `.map((group, i) => i === 0 ? group.splice(0, 0, ${extsText}) && group : group)`);
-				tsc = replace(tsc, /allSupportedExtensions = .*(?=;)/, s => s + `.map((group, i) => i === 0 ? group.splice(0, 0, ${extsText}) && group : group)`);
-			}
-			// Use to emit basename.xxx to basename.d.ts instead of basename.xxx.d.ts
-			if (extraExtensionsToRemove.length) {
-				const extsText = extraExtensionsToRemove.map(ext => `"${ext}"`).join(', ');
-				tsc = replace(tsc, /extensionsToRemove = .*(?=;)/, s => s + `.concat([${extsText}])`);
-			}
-			// Support for basename.xxx to basename.xxx.d.ts
-			if (needPatchExtenstions.length) {
-				const extsText = needPatchExtenstions.map(ext => `"${ext}"`).join(', ');
-				tsc = replace(tsc, /function changeExtension\(/, s => `function changeExtension(path, newExtension) {
-					return [${extsText}].some(ext => path.endsWith(ext))
-						? path + newExtension
-						: _changeExtension(path, newExtension)
-					}\n` + s.replace('changeExtension', '_changeExtension'));
-			}
-
-			// proxy createProgram
-			tsc = replace(tsc, /function createProgram\(.+\) {/, s =>
-				`var createProgram = require(${JSON.stringify(proxyApiPath)}).proxyCreateProgram(`
-				+ [
-					`new Proxy({}, { get(_target, p, _receiver) { return eval(p); } } )`,
-					`_createProgram`,
-					`require(${JSON.stringify(__filename)}).getLanguagePlugins`,
-				].join(', ')
-				+ `);\n`
-				+ s.replace('createProgram', '_createProgram')
-			);
-
-			return tsc;
+			return replaceTscContent(tsc, proxyApiPath, extraSupportedExtensions, extraExtensionsToRemove);
 		}
 		return (readFileSync as any)(...args);
 	};
