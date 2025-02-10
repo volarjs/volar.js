749afc5708939d2cf77aee50f16a2d21c067d1dd
machty <machty@gmail.com>
2025-02-10 17:24:36 -0500

    write docs

--- packages/typescript/lib/quickstart/createLanguageServicePlugin.ts
+++ packages/typescript/lib/quickstart/createLanguageServicePlugin.ts
@@ -3,6 +3,12 @@ import { createProxyLanguageService } from '../node/proxyLanguageService';
 import { createLanguageCommon, isHasAlreadyDecoratedLanguageService, makeGetExternalFiles, projectExternalFileExtensions } from './languageServicePluginCommon';
 import type { createPluginCallbackSync } from './languageServicePluginCommon';
 
+/**
+ * Creates and returns a TS Service Plugin using Volar primitives.
+ *
+ * See https://github.com/microsoft/TypeScript/wiki/Writing-a-Language-Service-Plugin for
+ * more information.
+ */
 export function createLanguageServicePlugin(
 	createPluginCallback: createPluginCallbackSync
 ): ts.server.PluginModuleFactory {
@@ -16,6 +22,9 @@ export function createLanguageServicePlugin(
 					const extensions = createPluginResult.languagePlugins
 						.map(plugin => plugin.typescript?.extraFileExtensions.map(ext => '.' + ext.extension) ?? [])
 						.flat();
+
+					// TODO: this logic does not seem to appear in the async variant
+					// (createAsyncLanguageServicePlugin)... bug?
 					projectExternalFileExtensions.set(info.project, extensions);
 
 					const { proxy, initialize } = createProxyLanguageService(info.languageService);
